d323773a7f47174fab753def96c4ad858cf98ceb
MaDXploit <Sekai-No-Saiba@users.noreply.github.com>
2023-06-08 00:43:10 -0400

    Second commit - class transformation

---
+++ SDK_Laboratoire1_David_Guevremont_Smith/SDK_Laboratoire1_David_Guevremont_Smith/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,44 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+        public string Transformer(string lettre)
+        {
+            string note = "";
+
+            switch (lettre.ToUpper())
+            {
+                case "A":
+                    note = "DO";
+                    break;
+                case "B":
+                    note = "RÉ";
+                    break;
+                case "C":
+                    note = "MI";
+                    break;
+                case "D":
+                    note = "FA";
+                    break;
+                case "E":
+                    note = "SOL";
+                    break;
+                case "F":
+                    note = "LA";
+                    break;
+                case "G":
+                    note = "SI";
+                    break;
+                default:
+                    // Cas où la lettre n'est pas valide
+                    note = "Note non disponible";
+                    break;
+            }
+
+            return note;
+        }
+
+
+
+    }
+}
