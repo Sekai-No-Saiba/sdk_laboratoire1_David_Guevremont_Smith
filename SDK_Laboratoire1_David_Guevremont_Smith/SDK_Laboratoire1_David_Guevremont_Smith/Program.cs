98ad8bf692bf8d6f620c2d06d3dd7ef01df44102
MaDXploit <Sekai-No-Saiba@users.noreply.github.com>
2023-06-15 21:59:19 -0400

    antoine input
    guide

--- SDK_Laboratoire1_David_Guevremont_Smith/SDK_Laboratoire1_David_Guevremont_Smith/Program.cs
+++ SDK_Laboratoire1_David_Guevremont_Smith/SDK_Laboratoire1_David_Guevremont_Smith/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LibrairieConversionNote;
+
+var lib = new ConversionAlphabetiqueToNote();
+lib.
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
